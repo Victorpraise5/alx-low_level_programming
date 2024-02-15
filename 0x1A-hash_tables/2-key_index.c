d8bc651dbbbad9b6b4870c5bda04b59be3bf0255
Victorpraise <victorpraise949@gmail.com>
2024-02-15 00:21:25 -0800

    task3

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives index of a key
+ * @key: key of the key/pair value
+ * @size: size of the array
+ * Return: index of the key
+ */
+unsigned long int key_index(const unsigned char *key,unsigned long int size)
+{
+	return(hash_djb2(key) % size);
+}
